b53839771bc7e7c0a5124db236f41c78ca2a76e5
yutuer <yutuerzf@163.com>
2017-10-31 00:08:28 +0800

    template

---
+++ src/main/java/com/yutuer/springBootDemo/ReadingListRepository.java
@@ -0,0 +1,12 @@
+package com.yutuer.springBootDemo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.yutuer.springBootDemo.bean.Book;
+
+public interface ReadingListRepository extends JpaRepository<Book, Long>{
+	
+	List<Book> findByReader(String reader);
+}
